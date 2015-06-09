ea63fb4b802949ca530809686705a24bbfd5991b
Wig C <onlywig@gmail.com>
2015-06-09 23:42:00 +0800

    Initial BT web.

--- main.py
+++ main.py
@@ -2,43 +2,46 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 import subprocess
+import sys
+import os
+import time
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET' , 'POST'])
 def main():
+    disk_size=list()
+    proc = subprocess.Popen(['./mnt_home','-c'], stdout=subprocess.PIPE)
+    time.sleep(1)
+    sc = open('./disk_list', 'r')
+    for line in sc.readlines():
+        disk_size.append(line.split("\n"))
 
-    if request.method == 'POST':
-        p_time2=request.form['date']
-
-        proc = subprocess.Popen(["echo '0616' | sudo -S date --set '%s' " % p_time2, ""], stdout=subprocess.PIPE, shell=True)
-        proc.communicate()
-
-    ip = request.remote_addr
-
-    proc = subprocess.Popen(['date','+ %Y/%m/%d-%H:%M'], stdout=subprocess.PIPE)
-    (time1, err) = proc.communicate()
-
-    proc = subprocess.Popen(['./get_data','-u'], stdout=subprocess.PIPE)
-    (user, err) = proc.communicate()
-
-    proc = subprocess.Popen(['./get_data','-p'], stdout=subprocess.PIPE)
-    (pw, err) = proc.communicate()
-
-    proc = subprocess.Popen(['./get_data','-l'], stdout=subprocess.PIPE)
-    (des_ip, err) = proc.communicate()
-
-    proc = subprocess.Popen(['arp'], stdout=subprocess.PIPE)
-    (message, err) = proc.communicate()
-    message=message.split("\n")
-
+    sc.close()
     templateData = {
-        'ip': ip,
-        'time1': time1,
-        'user': user,
-        'pw' : pw,
-        'message': message,
-        'des_ip': des_ip
+        'disk_size': disk_size
+    }
+
+    return render_template('index.html',**templateData)
+
+@app.route("/<mount>" ,methods=['GET' , 'POST'])
+def mount(mount):
+    disk_size=list()
+    proc = subprocess.Popen(['./mnt_home','-c'], stdout=subprocess.PIPE)
+
+    time.sleep(1)
+    sc = open('./disk_list', 'r')
+    for line in sc.readlines():
+        disk_size.append(line.split("\n"))
+    sc.close()
+
+    mnt_disk=request.form['mnt_disk']
+    proc = subprocess.Popen(["./mnt_home -m %s" % mnt_disk, ""], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    message=out
+    templateData = {
+        'disk_size': disk_size,
+        'message'  : message
     }
 
     return render_template('index.html',**templateData)
